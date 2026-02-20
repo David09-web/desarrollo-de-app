d525abd408b25a4c9215e3d4505fb89b03c5dc6a
David09_web <david_ospinagu@fet.edu.co>
2026-02-20 09:45:28 -0500

    Implementar lista de tareas en Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importante para el [(ngModel)]
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [FormsModule], // Necesario para manejar inputs
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('holamundo');
+  nuevaTarea: string = '';
+  listaTareas: string[] = [];
+
+  agregarTarea() {
+    if (this.nuevaTarea.trim() !== "") {
+      this.listaTareas.push(this.nuevaTarea);
+      this.nuevaTarea = ''; // Limpiamos el input
+    }
+  }
 }
